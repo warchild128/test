394eb823900314ee1084d8f628afd993dc12e6bf
warchild128 <>
2019-12-03 17:09:30 +0200

    test

---
+++ emitter/tests/example.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+
+describe('#sum()', function() {
+
+    context('without arguments', function() {
+        it('should return 0', function() {
+            expect(0).to.equal(0)
+        })
+    });
+
+});
